3ba3d7911b1673acfbdae7409bc08b120dfdd564
Paulo Alexandre <paulofore@gmail.com>
2018-12-20 20:38:26 -0200

    usar em casa

--- src/com/strongloop/android/remoting/adapters/RestAdapter.java
+++ src/com/strongloop/android/remoting/adapters/RestAdapter.java
@@ -416,7 +416,7 @@ public class RestAdapter extends Adapter {
 
    
             ProxyServer proxy = new ProxyServer("10.21.7.10",82, "tr626987", "mclaren3");
-            request.setProxyServer(proxy);
+            //request.setProxyServer(proxy);
             
             if ("GET".equalsIgnoreCase(method)) {
             	
